a49b4cb33aa1d7736a7ada422a39cde99b6045f5
Abhisar Ekka <abhisar4@gmail.com>
2021-02-04 16:30:56 +0530

    AbhisatTask2b

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "gouthamm.h"
+#include "abhisar.h"
 
 int main()
 {
     printf("Hello world!\n");
     print_gouthamm();
+    print_abhisar();
     return 0;
 }
---
+++ src/abhisar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "abhisar.h"
+
+void print_abhisar()
+{
+    printf("This is Abhisar\n");
+}
